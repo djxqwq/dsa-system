6357f25588901fccb084fdcb5a36adaea26752b5
lzk <3148644425@qq.com>
2026-03-11 11:27:32 +0800

    1.0

---
+++ src/main/java/com/djxlzk/dsasystem/DsaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.djxlzk.dsasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsaSystemApplication.class, args);
+    }
+
+}
