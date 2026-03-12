15fb48bb1e8e614518c1c3b3c2f03efd6ad0f508
lzk <3148644425@qq.com>
2026-03-12 15:34:32 +0800

    1.4实体类+DTO开发

--- src/main/java/com/djxlzk/dsasystem/entity/Coach.java
+++ src/main/java/com/djxlzk/dsasystem/entity/Coach.java
@@ -7,15 +7,45 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * 教练实体类
+ */
 @Data
 @TableName("coach")
 public class Coach {
+    /**
+     * 主键ID
+     */
     @TableId(type = IdType.AUTO)
     private Long id;
+
+    /**
+     * 教练编号
+     */
     private String coachNo;
+
+    /**
+     * 手机号
+     */
     private String mobile;
+
+    /**
+     * 密码
+     */
     private String password;
+
+    /**
+     * 姓名
+     */
     private String name;
+
+    /**
+     * 创建时间
+     */
     private LocalDateTime createTime;
+
+    /**
+     * 状态（0：禁用，1：启用）
+     */
     private Integer status;
 }
--- src/main/java/com/djxlzk/dsasystem/entity/Student.java
+++ src/main/java/com/djxlzk/dsasystem/entity/Student.java
@@ -7,15 +7,45 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * 学员实体类
+ */
 @Data
 @TableName("student")
 public class Student {
+    /**
+     * 主键ID
+     */
     @TableId(type = IdType.AUTO)
     private Long id;
+
+    /**
+     * 手机号
+     */
     private String mobile;
+
+    /**
+     * 密码
+     */
     private String password;
+
+    /**
+     * 用户名
+     */
     private String userName;
+
+    /**
+     * 创建时间
+     */
     private LocalDateTime createTime;
+
+    /**
+     * 状态（0：禁用，1：启用）
+     */
     private Integer status;
+
+    /**
+     * 爽约次数
+     */
     private Integer noShowCount;
 }
