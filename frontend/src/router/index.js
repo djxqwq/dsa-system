65050976077dac5cde94b72603a58a696c440353
djxqwq <djxqwq@github.com>
2026-04-26 16:30:01 +0800

    7.0.7修改前端配置适配VPS部署

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
 const LoginView = () => import('../views/LoginView.vue')
@@ -61,7 +61,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
